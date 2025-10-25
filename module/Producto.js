a67ad09812c0cade57779b70e3a85d6a5be03b04
LuisDDB <luisddb1706@gmail.com>
2025-10-24 20:49:40 -0700

    Se creo las rutas, controlador, data, model de producto y se corrigio el archivo servidor

---
+++ module/Producto.js
@@ -0,0 +1,11 @@
+class Producto{
+    constructor(id,nombre,precio,stock,descripcion,marca){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio =precio;
+        this.stock = stock;
+        this.descripcion = descripcion;
+        this.marca = marca;
+    }
+}
+module.exports= Producto;
--- servidro.js
+++ servidro.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const app= express();
 const port = 3000;
+const rutasProducto = require("./router/productoRouter")
 
 app.use(express.json());
 
-app.get("/saludo", (req, res) =>{
-    res.send("hola mundo");
-});
-app.post('/enviarmensaje', (req, res) => {
-  const { texto } = req.body;
-  if (!texto) {
-    return res.status(400).json({ error: 'Falta el campo texto en el cuerpo de la petición' });
-  }
-  res.json({ textoRecibido: texto });
-});
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Error interno del servidor' });
-});
+app.use('/api', rutasProducto)
+
 
 app.listen(port, () => {
   console.log(`Servidor iniciado correctamente desde: http://localhost:${port}`);
